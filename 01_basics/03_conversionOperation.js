1f13afbdfe3232ae98a6e65ba1da1a3968d53fff
ajeetsingh62688-rgb <ajeetsingh62688@gmail.com>
2026-07-28 10:33:45 +0000

    Conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score =undefined
+// console.log (typeof score);
+// console.log (typeof (score));
+
+// let valueInNumber = Number (score)
+// console.log(typeof valueInNumber );
+// console.log (valueInNumber);
+
+
+// "33"=> 33
+// "33abc"=> NaN
+// true => 1 ; false => 0
+
+
+// let isLoggedIn = "Ajeet"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log (booleanIsLoggedIn);
+
+// 1 =>true ; 0=>False
+//""=> false 
+//"Ajeet"=> true
+
+let someNumber =33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
