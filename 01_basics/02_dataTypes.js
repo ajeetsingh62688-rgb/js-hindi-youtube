d01a1bdc5f56e7d2462b3cf7083eb205c0b459b3
ajeetsingh62688-rgb <ajeetsingh62688@gmail.com>
2026-07-28 09:14:46 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello World!");// we are using nodejs ,not browser 
+
+console.log (3+3); console.log("Ajeet")
+// code readability is very important in programming.
+
+let name  = "Ajeet"
+let age = 18
+let isLoggedIn = true
+let state = null
+
+// number=> 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false 
+// null => standalone value
+// undefined=> 
+// symbole => unique value
+
+
+
+// object 
+
+
+console.log (typeof "Ajeet");
+console.log (typeof null);
