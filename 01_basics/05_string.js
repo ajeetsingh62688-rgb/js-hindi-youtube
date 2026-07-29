9f78700586c818df5c102f2e96c24dc282a675cc
ajeetsingh62688-rgb <ajeetsingh62688@gmail.com>
2026-07-29 06:20:44 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,36 @@
+const name ="Ajeet"
+const repoCount = 50
+
+// console.log(name + repoCount+ "value");
+
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String("Ajeet")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("e"));
+
+
+const newString = gameName.substring(0,3)
+// console.log(newString);
+
+const anotherString = gameName.slice(-4,3)
+// console.log(anotherString);
+
+const newString1 = "   hitesh "
+console.log(newString1);
+console.log(newString1.trim());
+
+const url = "https://www.google.com/search?q=javascript"
+console.log(url.replace('google', 'Ajeet'));
+
+console.log(url.includes('google'));
+
+
