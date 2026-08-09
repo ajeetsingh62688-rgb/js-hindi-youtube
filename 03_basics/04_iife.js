c43ccddbd8cdd0794452670c56dc12216d1f5798
ajeetsingh62688-rgb <ajeetsingh62688@gmail.com>
2026-08-09 02:42:34 +0000

    iffe

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,23 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function chai (){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+
+}) ();
+
+
+
+( () => {
+    console.log(`DB CONNECTED TWO`);
+
+}) ();
+
+
+
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+
+}) ("hitesh" );
+
