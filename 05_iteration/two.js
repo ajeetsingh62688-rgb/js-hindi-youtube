38d3f274cf58cef6d3aca6a02265ecb1fbaced4d
ajeetsingh62688-rgb <ajeetsingh62688@gmail.com>
2026-08-12 07:38:18 +0000

    while loop and do while

---
+++ 05_iteration/two.js
@@ -0,0 +1,25 @@
+
+
+let index = 0;
+while (index<= 10) {
+    // console.log (`value of index is ${index}`);
+    index = index +2    
+}
+
+
+
+let MyArray =["flash","batman","superman"]
+let arr = 0
+while(arr < MyArray.length) {
+    // console.log(`value of arr is ${MyArray[arr]}`);
+    arr = arr + 1
+}
+
+
+
+let score =1 
+do {
+    console.log(`score is ${score}`);
+    score = score + 1
+}
+while (score <= 10);
